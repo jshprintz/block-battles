52c534a76094180c1b5a881fbb4ae600c2ec6fd6
Jason Shprintz <100168347+jshprintz@users.noreply.github.com>
2025-07-05 13:37:23 -0700

    Wire up buttons (#1)
    * Routing wired up
    
    * Minor name changes
    
    * Buttons are now working

---
+++ src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+import { COLORS, FlexCol } from "../../styles/core/styles";
+import { Link } from "react-router-dom";
+
+function LandingPage() {
+  return (
+    <Container>
+      <LandingPageContainer>
+        <HeaderContainer>
+          <h1>Dungeon Battles</h1>
+        </HeaderContainer>
+        <Menu>
+          <StartButton to="/start">
+            <p>Start</p>
+          </StartButton>
+          <RegularMenuButton to="/rules">
+            <p>Rules</p>
+          </RegularMenuButton>
+        </Menu>
+      </LandingPageContainer>
+    </Container>
+  );
+}
+
+const Container = styled(FlexCol)`
+  height: 99dvh;
+  font-size: 100%;
+  overflow: hidden;
+
+  font-family: "Megrim", system-ui;
+  font-weight: 400;
+  font-style: normal;
+`;
+
+const LandingPageContainer = styled(FlexCol)`
+  justify-content: space-around;
+
+  color: white;
+  background-color: black;
+`;
+
+const HeaderContainer = styled(FlexCol)`
+  height: auto;
+  text-align: center;
+  font-size: 300%;
+`;
+
+const Menu = styled(FlexCol)`
+  height: 50%;
+  width: 75%;
+  justify-content: space-evenly;
+  border-radius: 5px;
+`;
+
+const MenuButton = styled(Link)`
+  height: 25%;
+  width: 50%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  color: white;
+  text-decoration: none;
+
+  border-radius: 5px;
+  transition: transform 200ms ease-in;
+  cursor: pointer;
+  font-size: 300%;
+
+  &:hover {
+    transform: scale(1.02);
+  }
+`;
+
+const StartButton = styled(MenuButton)`
+  background-image: ${COLORS.START_BTN};
+  font-size: 350%;
+`;
+
+const RegularMenuButton = styled(MenuButton)`
+  background-image: ${COLORS.REG_BTN};
+`;
+
+export default LandingPage;
