4a6816dddc14fe600910283050972072d6bb6a0c
jshprintz <jshprintz@gmail.com>
2025-07-11 18:49:25 -0700

    Update StartPage.tsx

--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -33,11 +33,26 @@ const StartPage = () => {
         <HiChevronRight size={150} style={{ cursor: "pointer" }} />
       </SelectionBoxContainer>
       <TeamBox>
-        <TeamCard></TeamCard>
-        <TeamCard></TeamCard>
-        <TeamCard></TeamCard>
-        <TeamCard></TeamCard>
-        <TeamCard></TeamCard>
+        <TeamCard>
+          <div>Image</div>
+          <div>Class</div>
+        </TeamCard>
+        <TeamCard>
+          <div>Image</div>
+          <div>Class</div>
+        </TeamCard>
+        <TeamCard>
+          <div>Image</div>
+          <div>Class</div>
+        </TeamCard>
+        <TeamCard>
+          <div>Image</div>
+          <div>Class</div>
+        </TeamCard>
+        <TeamCard>
+          <div>Image</div>
+          <div>Class</div>
+        </TeamCard>
       </TeamBox>
       <HomeButton to="/">Home</HomeButton>
     </StartPageContainer>
@@ -111,7 +126,7 @@ const TeamCard = styled(FlexCol)`
   width: 15%;
   height: 95%;
   border: 1px solid white;
-  align-items: space-between;
+  justify-content: space-around;
   border-radius: 5px;
 `;
 
