38d7fd0bd27466f39a094a18d41439e70a8f4d4c
Jason Shprintz <100168347+jshprintz@users.noreply.github.com>
2025-08-10 17:47:40 -0700

    Random Skill working! (#9)
    Using the store as the param. This way I can use it for the user too, or any other random skill need in the future.

--- src/helpers/randomSkillDistribution.ts
+++ src/helpers/randomSkillDistribution.ts
@@ -1,15 +1,19 @@
 import {
   MAX_NUM_OF_ASSIGNED_SKILLS,
-  NUM_OF_BONUS_SKILL_POINTS,
   NUM_OF_WARRIORS_ON_TEAM,
 } from "../Constants";
-import { IWarrior } from "../types/core";
+import { ISkillTree, IWarrior } from "../types/core";
 
-const randomSkillDistribution = (
-  warriorList: IWarrior[],
-  bonusCount: number
-): IWarrior[] => {
-  while (bonusCount > 0) {
+interface IStoreProps {
+  assembledTeam: IWarrior[];
+  bonusSkillPointCount: number;
+  [key: string]: any;
+}
+
+const randomSkillDistribution = (store: IStoreProps) => {
+  const warriorList: IWarrior[] = store.assembledTeam;
+
+  while (store.bonusSkillPointCount > 0) {
     const randomWarriorIndex = Math.floor(
       Math.random() * NUM_OF_WARRIORS_ON_TEAM
     );
@@ -20,8 +24,10 @@ const randomSkillDistribution = (
       skillKeys[Math.floor(Math.random() * skillKeys.length)];
 
     if (warrior.skillTree[randomSkillKey] < MAX_NUM_OF_ASSIGNED_SKILLS) {
-      warrior.skillTree[randomSkillKey]++;
-      bonusCount--;
+      store.increaseWarriorSkill(
+        warrior.id,
+        randomSkillKey as keyof ISkillTree
+      );
     }
   }
 
